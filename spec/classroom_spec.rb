8ba9a768a560ffd5be10f43e2e0152a3498d71bd
abiodunraheem <raheemabiodun23@gmail.com>
2022-08-05 12:25:26 +0100

    Write test for classroom.rb class

--- spec/classroom_spec.rb
+++ spec/classroom_spec.rb
@@ -2,10 +2,10 @@ require './classroom'
 
 describe Classroom do
   before :each do
-    @classroom = Classroom.new('grade12B')
+    @classroom = Classroom.new('grade9B')
   end
 
   it 'should print the grade' do
-    expect(@classroom.label).to eq 'grade12B'
+    expect(@classroom.label).to eq 'grade9B'
   end
 end
