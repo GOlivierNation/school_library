69375f37e50af1d3f194d4ebfc3fa98e6713f139
abiodunraheem <raheemabiodun23@gmail.com>
2022-08-05 12:36:58 +0100

    Update test for teacher.rb class

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -3,14 +3,14 @@ require './rental'
 
 describe Book do
   before :each do
-    @book = Book.new nil, 'Freedom', 'Nelson'
+    @book = Book.new nil, 'title', 'authour'
   end
 
   it 'should print the book title' do
-    expect(@book.title).to eq 'Freedom'
+    expect(@book.title).to eq 'title'
   end
 
   it 'should print the book author' do
-    expect(@book.author).to eq 'Nelson'
+    expect(@book.author).to eq 'authour'
   end
 end
--- spec/decorator_spec.rb
+++ spec/decorator_spec.rb
@@ -11,7 +11,7 @@ describe Decorator do
   it 'Should display name' do
     expect(@person.correct_name).to eq 'maximulus'
   end
-  it 'should capitalize personne name' do
+  it 'should capitalize person name' do
     expect(@captalize.correct_name).to eq 'Maximulus'
   end
   it 'should slice if sting is more than 10 char' do
